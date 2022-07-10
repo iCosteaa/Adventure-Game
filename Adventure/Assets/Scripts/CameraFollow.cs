efbc25217d8cd568ff4d4bbc11d3c1686071efea
AdrianGolea <adyx102001@gmail.com>
2022-07-10 16:56:54 +0300

    Player Dodge, Camera Follow, Added Ground Layer

---
+++ Adventure/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+
+    public float smoothSpeed = 1f;
+    public Vector3 offset;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed /** Time.deltaTime*/);
+        transform.position = smoothPosition;
+
+        transform.LookAt(target);
+    }
+}
---
+++ Adventure/Assets/Scripts/PlayerDodge.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDodge : MonoBehaviour
+{
+
+    Rigidbody rb;
+    [SerializeField] float dodgeSpeed;
+    /*[SerializeField] float dodgeTime;*/
+    
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    
+    void Update()
+    {
+       
+       
+        if (Input.GetButtonUp("Dodge"))
+        {
+            float dodge = Input.GetAxis("Dodge");
+
+             rb.velocity = new Vector3(dodge, 0, 0);
+
+            
+            rb.MovePosition(transform.position + rb.velocity * /*Time.deltaTime **/ dodgeSpeed);
+            
+
+        }
+    }
+
+    /*IEnumerator Dodge()
+    {
+        float startTime = Time.time;
+
+        while(Time.time < startTime + dodgeTime)
+        {
+            rb.velocity = new Vector3(dodgeSpeed * Time.deltaTime,0,dodgeSpeed * Time.deltaTime);
+
+            yield return null;
+        }
+    }*/
+}
